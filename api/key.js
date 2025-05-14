e1819cfdb628777051fbb860013fe0005cfb9fb8
NTT-KEY <thaitrucfb4@gmail.com>
2025-05-14 11:17:26 +0700

    Update key.js

--- api/key.js
+++ api/key.js
@@ -4,7 +4,7 @@ const fs = require('fs');
 const app = express();
 const port = 3000;
 
-app.get('/key', (req, res) => {
+app.get('/Key', (req, res) => {
   const filePath = req.query.file;
 
   if (!filePath) {
